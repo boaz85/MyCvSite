066c07dcb490e2359477cb2393a5ac29ae30e807
boaz85@gmail.com <boaz85@gmail.com>
2016-08-06 22:24:02 +0300

    Play with scrollMagic

--- src/common/MainContainer.js
+++ src/common/MainContainer.js
@@ -2,14 +2,21 @@ import React from 'react';
 import TopBar from '../components/TopBar/TopBar';
 import Banner from '../components/Banner/Banner';
 import Skills from '../components/Skills/Skills';
+import ScrollMagic from "scrollmagic";
 
 export default class MainContainer extends React.Component {
+
+    constructor() {
+        super();
+        this.controller = new ScrollMagic.Controller();
+
+    }
 
     render() {
         return (
             <div>
                 <TopBar ref="topBarElement"/>
-                <Banner ref="bannerElement"/>
+                <Banner ref="bannerElement" controller={this.controller}/>
                 <Skills ref="skillsElement"/>
             </div>
         );
--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,15 +1,37 @@
 import React from "react";
 import styles from "./style.css";
 import portraitImg from "./portrait.jpg";
+import ScrollMagic from "scrollmagic";
+require('animation.gsap');
+require('debug.addIndicators');
+import TweenMax from "gsap";
 
 
 export default class Banner extends React.Component {
+
+    initScrollMagic(element) {
+        element.blurValue = 0;
+
+        function applyBlur()
+        {
+            TweenMax.set(element, {webkitFilter:"blur(" +element.blurValue + "px)"});
+        }
+        var tween = TweenMax.to(element, 1, {blurValue: 3, onUpdate:applyBlur});
+
+        // build scene
+        var height = element.offsetHeight;
+        var scene = new ScrollMagic.Scene({triggerElement: element, offset: height + 20, duration: 300})
+            .setTween(tween)
+            .addIndicators() // add indicators (requires plugin)
+            .addTo(this.props.controller);
+    }
+
 
     render() {
 
         return (
 
-            <div className={styles.container}>
+            <div ref={this.initScrollMagic.bind(this)} className={styles.container}>
                 <img src={portraitImg} className={styles.portrait}></img>
                 <div className={styles.textsContainer}>
                     <h2>Full Stack Web Developer</h2>
